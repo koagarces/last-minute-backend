f8b3111b95cc939daebde5592c8238ddd8398885
Koa Garces <koajgarces@yahoo.com>
2022-05-13 21:35:21 -0700

    created update event route

--- routes/EventRouter.js
+++ routes/EventRouter.js
@@ -3,6 +3,7 @@ const controllers = require("../controllers/eventController");
 
 Router.get("/:id", controllers.getEventById);
 Router.get("/", controllers.getAllEvents);
+Router.get("/user/:userId", controllers.getAllEventsByUserId);
 Router.post("/:userId", controllers.createEvent);
 Router.delete("/:userId/:id", controllers.DeleteEvent);
 Router.put("/:userId/:id", controllers.UpdateEvent);
