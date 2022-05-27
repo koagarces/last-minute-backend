107d27798e2e623860dc2b9a5e2b57fd70073c74
Koa Garces <koajgarces@yahoo.com>
2022-05-27 00:49:51 -0700

    added to server file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
+const logger = require("morgan");
 const app = express();
-
+const bodyParser = require("body-parser");
 const AppRouter = require("./routes/AppRouter");
 
 const PORT = process.env.PORT || 3001;
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(logger("dev"));
 app.get("/", (req, res) => res.json({ message: "Server Works" }));
 app.use("/", AppRouter);
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
+app.use(bodyParser.json());
